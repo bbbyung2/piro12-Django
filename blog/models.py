4e3ec5081242abb8a210f936fad57f0978f83efa
bbbyung2 <bhko0524@naver.com>
2020-01-20 19:46:15 +0900

    askdjango

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     author_name = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     content = models.TextField()
+    photo = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- shop/models.py
+++ shop/models.py
@@ -8,6 +8,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100, validators=[])
     desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
+    photo = models.ImageField(blank=True)
     is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
